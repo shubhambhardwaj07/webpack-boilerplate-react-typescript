63807637e9eeea25763813c5c381d518e57304cb
shubh <shubham998845@gmail.com>
2023-07-12 14:52:23 +0530

    extra things to have ------ babel-runtime,copyblugin,webpack-bundle-alalyzer

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,14 +1,15 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -17,27 +18,30 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
           },
         ],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'source', to: 'dest', noErrorOnMissing: true }],
     }),
   ],
-};
+}
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,15 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 //we can deine env varibale name by ourserves as well if not it will take mode as values
 // go to build folder and do npx serve
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.name": JSON.stringify("ShubhProd"),
+      'process.env.name': JSON.stringify('ShubhProd'),
     }),
+    new BundleAnalyzerPlugin(),
   ],
-};
+}
